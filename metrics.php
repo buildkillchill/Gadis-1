3f7a12231e8a5b082adc6c3c2eeafcd7908a9a07
halitalf <pwthomas95@zoho.com>
2018-04-30 18:50:45 -0500

    Trying to fix stuff

--- metrics.php
+++ metrics.php
@@ -1,4 +1,17 @@
 <?php
+	function convID($steamId)
+	{
+		$gameType = 0;
+		$authServer = 0;
+		$steamId = str_replace('STEAM_', '' ,$steamId);
+		$parts = explode(':', $steamId);
+		$gameType = $parts[0];
+		$authServer = $parts[1];
+		$clientId = $parts[2];
+		$res = bcadd((bcadd('76561197960265728', $authServer)), (bcmul($clientId, '2')));
+		$cid = str_replace('.0000000000', '', $res);
+		return $cid;
+	}
 	header("Content-Type: application/json");
 	require 'settings.php';
 	// 0 = Connected or Joined
@@ -30,14 +43,14 @@
 		if($conn->connect_errno) output(0x201, "MySQL connection failed.");
 		if($_GET["act"] == "0")
 		{
-			if(!$conn->query("INSERT INTO `metrics` (id) VALUES (".$_GET["id"].")")) output(0x2002, "MySQL query failed", $conn->errno, $conn->error);
-			if(!$conn->query("INSERT INTO `active` (id) VALUES (".$_GET["id"].")")) output(0x2002, "MySQL query failed", $conn->errno, $conn->error);
+			if(!$conn->query("INSERT INTO `metrics` (id) VALUES (".convId($_GET["id"]).")")) output(0x2002, "MySQL query failed", $conn->errno, $conn->error);
+			if(!$conn->query("INSERT INTO `active` (id) VALUES (".convId($_GET["id"]).")")) output(0x2002, "MySQL query failed", $conn->errno, $conn->error);
 			output(0x000, "Player connected");
 		}
 		elseif($_GET["act"] == "1")
 		{
-			if(!$conn->query("UPDATE `metrics` SET `disconnect`=NOW() WHERE `disconnect` IS NULL AND `id`=".$_GET["id"])) output(0x2002, "MySQL query failed.", $conn->errno, $conn->error);
-			if(!$conn->query("DELETE FROM `active` WHERE `id`=".$_GET["id"])) output(0x2002, "MySQL query failed", $conn->errno, $conn->error);
+			if(!$conn->query("UPDATE `metrics` SET `disconnect`=NOW() WHERE `disconnect` IS NULL AND `id`=".convId($_GET["id"]))) output(0x2002, "MySQL query failed.", $conn->errno, $conn->error);
+			if(!$conn->query("DELETE FROM `active` WHERE `id`=".convId($_GET["id"]))) output(0x2002, "MySQL query failed", $conn->errno, $conn->error);
 			output(0x000, "Player disconnected");
 		}
 		else output(0x000);
